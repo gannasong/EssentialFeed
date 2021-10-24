06459406981cfaa9d8b84c8e2de1edf648d927e4
Miguel <gannasong@gmail.com>
2021-10-24 22:21:45 +0800

    Enable random test execution order to guarantee there is no order dependency between test methods

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -9,14 +9,14 @@ import Foundation
 
 public class URLSessionHTTPClient: HTTPClient {
   private let session: URLSession
-  
+
   // 如果使用套件就在 init 時給預設值
   public init(session: URLSession = .shared) {
     self.session = session
   }
-  
+
   private struct UnexpectedValuesRepresentation: Error {}
-  
+
   public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
     session.dataTask(with: url) { data, response, error in
       if let error = error {
